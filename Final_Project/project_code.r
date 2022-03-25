45fa46cdcf989f6977db7803b2c025fceae538bf
Jiacheng Wang <63186896+whyjackeys0n@users.noreply.github.com>
2022-03-25 15:55:45 -0400

    Update project_code.r

--- Final_Project/project_code.r
+++ Final_Project/project_code.r
@@ -1,5 +1,7 @@
 ### CICS 197R - Final Project - Jiacheng Wang ###
 
+library(ggplot2)
+
 # 1. Load the lithium batteries data
 libattery <- read.csv("lithium-ion batteries.csv")
 head(libattery)
@@ -15,26 +17,41 @@ names(lbdata)[names(lbdata) == "Density..gm.cc."] <- "Density"
 names(lbdata)[names(lbdata) == "Has.Bandstructure"] <- "Bandstructure"
 names(lbdata)[names(lbdata) == "Crystal.System"] <- "Crystal"
 
-# 4. Find the total count of climbs in each season
-aggregate(E_Formation ~ Spacegroup, data = lbdata, FUN = mean)
+# 4. Aggregate Energy data by Spacegroup based with max or min method
+aggregate(E_Formation ~ Spacegroup, data = lbdata, FUN = max)
+aggregate(E_Above_Hull ~ Spacegroup, data = lbdata, FUN = min)
 
-# 5. Linear regression model for Volume to Nsite
+# 5. Plot distribution for Band Gap
+print(ggplot(lbdata, aes(x=Band_Gap)) + geom_histogram(binwidth=.5))
+
+# 6. Linear regression model for Volume to Nsite
 data_VN <- data.frame(Volume = lbdata$Volume, Nsites = lbdata$Nsites)
 
-# 6. Fit linear regression model to dataset and view model summary
+# 7. Fit linear regression model to dataset and view model summary
 linear_model <- lm(Volume ~ Nsites, data=data_VN)
-summary(linear_model)
+print(summary(linear_model))
 
-# 7. Create plot to visualize fitted linear regression model
-library(ggplot2)
-p <- ggplot(data,aes(x, y)) + 
+# 8. Create plot to visualize fitted linear regression model
+p1 <- ggplot(data_VN,aes(Nsites, Volume)) + 
   geom_point() + 
   geom_smooth(formula = y ~ x, method='lm', color='turquoise4') +
   theme_minimal() +
-  labs(x='Nsites', y='Volume', title='Linear Regression Plot') +
-  theme(plot.title = element_text(hjust=0.5, size=20, face='bold')) 
-print(p)
+  labs(x='Nsites', y='Volume', 
+       title='Linear Regression Fit for Volume by Nsites') +
+  theme(plot.title = element_text(hjust=0.5, size=15, face='bold')) 
+print(p1)
 
-# 8. Add new column of Band
-data_VNB <- cbind(data_VN, Bandstructure = lbdata$Bandstructure)
+# 9. Add new column of Density
+data_VND <- cbind(data_VN, Density = lbdata$Density, Crystal = lbdata$Crystal)
+
+# 10. Function for calculating the average density and converting the unit
+gmcc2kgmcm <- function(gmcc){
+  kgmcm <- mean(gmcc) * 1000
+  return(kgmcm)
+}
+
+# 11. Aggregate Density by Crystal System with the defined function
+print(aggregate(data_VND$Density, by = list(data_VND$Crystal), FUN=gmcc2kgmcm))
+
+
 
